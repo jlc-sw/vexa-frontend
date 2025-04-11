f6504d6b757e0fd619026238e5cc7c99223aeafd
jlc-sw <131212713+jlc-sw@users.noreply.github.com>
2025-04-11 12:50:46 +0100

    Set static backend domain

--- pages/1_How_It_Works.py
+++ pages/1_How_It_Works.py
@@ -29,7 +29,7 @@ logo = Image.open("assets/vexa_logo.png")
 
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image(logo, width=90)
+    st.image(logo, width=120)
 with col2:
     st.markdown("## Welcome to Vexa")
 
